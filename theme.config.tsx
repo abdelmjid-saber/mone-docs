bbc908ff82bf96702a407ab41cba460931038459
Abdelmjid Saber <87904671+abdelmjid-saber@users.noreply.github.com>
2023-11-11 14:52:58 +0300

    Add files via upload

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Mone</span>,
+  project: {
+    link: 'https://github.com/abdelmjid-saber/mone-docs',
+  },
+  docsRepositoryBase: 'https://github.com/abdelmjid-saber/mone-docs',
+  footer: {
+    text: 'Mone Docs',
+  },
+}
+
+export default config
